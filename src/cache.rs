768784ec380bd54ceff6447c7ab6508bee1460ca
Quaternions <krakow20@gmail.com>
2025-04-25 16:54:48 -0700

    save the allocation in intern_str

--- src/cache.rs
+++ src/cache.rs
@@ -1,5 +1,5 @@
 use crate::hash_str::make_hash;
-use crate::hash_str::HashStr;
+use crate::hash_str::{HashStr,SIZE_U64};
 use hashbrown::HashTable;
 
 /// "Host" backing storage for cached HashStrs.
@@ -27,9 +27,9 @@ impl<'str> HashStrCache<'str>{
 	}
 	/// Calculate the hash of a &str and fetch an existing HashStr.
 	#[inline]
-	pub fn get_str(&self,string:&str)->Option<&'str HashStr>{
-		let hash=make_hash(string);
-		self.get_with_hash(hash,string)
+	pub fn get_str(&self,str:&str)->Option<&'str HashStr>{
+		let hash=make_hash(str);
+		self.get_with_hash(hash,str)
 	}
 	/// Fetch an existing HashStr using the precalculated hash.
 	#[inline]
@@ -37,37 +37,47 @@ impl<'str> HashStrCache<'str>{
 		self.get_with_hash(hash_str.precomputed_hash(),hash_str.as_str())
 	}
 	#[inline]
-	fn get_with_hash(&self,hash:u64,string:&str)->Option<&'str HashStr>{
-		self.entries.find(hash,|&s|s.as_str()==string).copied()
+	fn get_with_hash(&self,hash:u64,str:&str)->Option<&'str HashStr>{
+		self.entries.find(hash,|&s|s.as_str()==str).copied()
 	}
 	/// Calculate the hash of a &str and intern it into the HashStrHost.
 	/// Returns the newly allocated HashStr or an existing one if there was one.
 	#[inline]
 	pub fn intern_str(&mut self,host:&'str HashStrHost,str:&str)->&'str HashStr{
-		// TODO: avoid allocation
-		let mut string=String::with_capacity(str.len()+crate::hash_str::SIZE_U64);
-		string.push_str(str);
-		let hash_str=&*HashStr::anonymous(string);
-		self.intern_hash_str(host,hash_str)
+		let hash=make_hash(str);
+		self.intern_with_hash(host,hash,str)
 	}
 	/// Intern the provided HashStr into the HashStrHost using the precalculated hash.
 	/// Returns the newly allocated HashStr or an existing one if there was one.
 	#[inline]
 	pub fn intern_hash_str(&mut self,host:&'str HashStrHost,hash_str:&HashStr)->&'str HashStr{
+		self.intern_with_hash(host,hash_str.precomputed_hash(),hash_str.as_str())
+	}
+	#[inline]
+	fn intern_with_hash(&mut self,host:&'str HashStrHost,hash:u64,str:&str)->&'str HashStr{
 		// check exists
-		if let Some(hash_str)=self.get_hash_str(hash_str){
+		if let Some(hash_str)=self.get_with_hash(hash,str){
 			return hash_str;
 		}
 
-		// alloc new hash_str
-		let new_hash_str_bytes=host.0.alloc_slice_copy(hash_str.as_hash_str_bytes());
-		// SAFETY: the bytes returned from the alloc
-		// are copied from the bytes fed into the alloc
+		let hash_str_len=SIZE_U64+str.len();
+		let layout=bumpalo::core_alloc::alloc::Layout::from_size_align(hash_str_len,SIZE_U64).unwrap();
+		// alloc empty bytes for new HashStr
+		let new_hash_str_bytes_ptr=host.0.alloc_layout(layout).as_ptr();
+		// SAFETY: bumpalo panics if allocation fails
+		// meaning ptr is always non-null
+		let new_hash_str_bytes=unsafe{core::slice::from_raw_parts_mut(
+			new_hash_str_bytes_ptr,
+			hash_str_len
+		)};
+		new_hash_str_bytes[..SIZE_U64].copy_from_slice(&hash.to_ne_bytes());
+		new_hash_str_bytes[SIZE_U64..].copy_from_slice(str.as_bytes());
+		// SAFETY: A valid HashStr is constructed in new_hash_str_bytes
 		let new_hash_str=unsafe{HashStr::ref_from_bytes(new_hash_str_bytes)};
 
 		// insert into entries
 		self.entries.insert_unique(
-			hash_str.precomputed_hash(),
+			hash,
 			new_hash_str,
 			|hash_str|hash_str.precomputed_hash()
 		).get()
