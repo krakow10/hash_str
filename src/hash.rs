10393b53051c3446cabe2e5a11692261db6839ef
Quaternions <krakow20@gmail.com>
2025-05-02 12:30:05 -0700

    implement a stupid randomstate this is probably a terrible idea but I just want to test if I'm getting quadratic behaviour in the serialization

--- src/hash.rs
+++ src/hash.rs
@@ -1,6 +1,6 @@
 use crate::hash_str::HashStr;
 use std::collections::{HashMap,HashSet};
-use core::hash::{BuildHasher,BuildHasherDefault,Hash,Hasher};
+use core::hash::{BuildHasher,Hash,Hasher};
 
 pub(crate) fn make_hash(value:&str)->u64{
 	let not_random_state=ahash::RandomState::with_seeds(0,0,0,0);
@@ -20,50 +20,88 @@ impl Hash for HashStr{
 
 /// A standard `HashMap` using `&HashStr` as the key type with a custom `Hasher`
 /// that just uses the precomputed hash for speed instead of calculating it.
-pub type HashStrMap<'a,V>=HashMap<&'a HashStr,V,BuildHasherDefault<IdentityHasher>>;
+pub type HashStrMap<'a,V>=HashMap<&'a HashStr,V,RandomState>;
 
 /// A standard `HashSet` using `&HashStr` as the key type with a custom `Hasher`
 /// that just uses the precomputed hash for speed instead of calculating it.
-pub type HashStrSet<'a>=HashSet<&'a HashStr,BuildHasherDefault<IdentityHasher>>;
+pub type HashStrSet<'a>=HashSet<&'a HashStr,RandomState>;
 
-/// The worst hasher in the world -- the identity hasher.
+// generate a random u64 for each HashMap to avoid quadratic behaviour
 #[doc(hidden)]
-#[derive(Default)]
-pub struct IdentityHasher {
-    hash: u64,
+#[derive(Copy,Clone,Debug)]
+pub struct RandomState{
+	random_state:u64,
+}
+impl Default for RandomState{
+	fn default()->Self{
+		// hashbrown does this
+		let mut seed=0;
+		let ptr=core::ptr::addr_of!(seed);
+		let mut hasher=ahash::AHasher::default();
+		hasher.write_usize(ptr as usize);
+		seed=hasher.finish();
+		Self{
+			random_state:seed,
+		}
+	}
 }
 
-impl Hasher for IdentityHasher {
+impl BuildHasher for RandomState{
+	type Hasher=XORHasher;
+	fn build_hasher(&self)->Self::Hasher{
+		XORHasher{
+			random_state:self.random_state,
+			hash:0,
+		}
+	}
+}
+
+/// XOR the provided precomputed hash with the random state
+/// so creating a hash table from another doesn't have
+/// accidental quadratic time complexity.
+#[doc(hidden)]
+pub struct XORHasher{
+	random_state:u64,
+    hash:u64,
+}
+
+impl Hasher for XORHasher{
 	#[inline]
-    fn write_u64(&mut self, value: u64) {
-        self.hash = value;
+    fn write_u64(&mut self,value:u64){
+        self.hash=value^self.random_state;
     }
     #[inline]
-    fn write(&mut self, bytes: &[u8]) {
-        self.hash = u64::from_ne_bytes(bytes.try_into().unwrap());
+    fn write(&mut self,_bytes:&[u8]){
+    	unreachable!();
     }
     #[inline]
-    fn finish(&self) -> u64 {
+    fn finish(&self)->u64{
         self.hash
     }
 }
 
+impl Default for XORHasher{
+	fn default()->Self{
+		RandomState::default().build_hasher()
+	}
+}
+
 #[test]
-fn test_hashing() {
+fn test_hashing(){
 	let u1=&*HashStr::anonymous("the quick brown fox".to_owned());
 	let u2=&*HashStr::anonymous("jumps over the lazy dog".to_owned());
 
-	let mut hasher = IdentityHasher::default();
+	let mut hasher=XORHasher::default();
 	u1.hash(&mut hasher);
-	assert_eq!(hasher.finish(), u1.precomputed_hash());
+	assert_eq!(hasher.finish(),u1.precomputed_hash());
 
-	let mut hasher = IdentityHasher::default();
+	let mut hasher=XORHasher::default();
 	u2.hash(&mut hasher);
-	assert_eq!(hasher.finish(), u2.precomputed_hash());
+	assert_eq!(hasher.finish(),u2.precomputed_hash());
 
-	let mut hm = HashStrMap::<u32>::default();
-	hm.insert(u1, 17);
-	hm.insert(u2, 42);
+	let mut hm=HashStrMap::<u32>::default();
+	hm.insert(u1,17);
+	hm.insert(u2,42);
 
 	assert_eq!(hm.get(u1), Some(&17));
 	assert_eq!(hm.get(u2), Some(&42));
